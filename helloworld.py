9af63fe774f7bdc50cd6485d86f7e4396dbf9d5b
matthewbruins <mebruins12@gmail.com>
2023-03-05 17:55:21 -0500

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World! {city}, {state}')
 
 
 def main():
     hello_world('NYC', 'New York')
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 69858193570ed6ceffe02e2412bacdb7e8491aab
-
 
 main()
